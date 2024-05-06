faa6cba399d5e650385a79356f26bb07412e702e
vlandero <vlad.andrei.rosu@gmail.com>
2024-05-07 00:05:53 +0300

    connect web3 with react

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { ethers } from 'ethers';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ FE/src/pages/Home/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import styles from "./home.module.css";
+const provider = new ethers.BrowserProvider(window.ethereum);
+
+export default function Home() {
+  const [account, setAccount] = useState<ethers.JsonRpcSigner | null>(null);
+  const [balance, setBalance] = useState<string | null>(null);
+  const [errText, setErrText] = useState<string | null>(null);
+
+  const getBalance = async () => {
+    if (account) {
+      const balance = await provider.getBalance(account.getAddress());
+      setBalance(balance.toString());
+    }
+  };
+
+  useEffect(() => {
+    getBalance();
+  }, [account]);
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        const accountSign = await provider.send("eth_requestAccounts", []);
+        const account = await provider.getSigner(accountSign[0]);
+        await provider.getBalance(account.getAddress());
+        setAccount(account);
+      } catch (err) {
+        console.log(err);
+        setErrText("Please allow the connection to proceed.");
+      }
+    } else {
+      setErrText("Please install a wallet extension.");
+    }
+  };
+  return (
+    <div>
+      <div>{account?.address}</div>
+      <button onClick={connectWallet}>Connect Wallet</button>
+      <div>
+        To disconnect, just remove the website from your metamask extension and
+        refresh the page.
+      </div>
+      <div>{balance}</div>
+      <div className={styles["err"]}>{errText}</div>
+    </div>
+  );
+}
